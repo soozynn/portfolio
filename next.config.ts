ff291bae87f3017b122f325081f52c43bdb49716
soozynn <junsuejin98@gmail.com>
2026-01-13 21:34:40 +0900

    Chore: initialize portfolio project structure

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US', 'ko'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'ko',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+  }
+}
 
 export default nextConfig;
