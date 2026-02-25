8d3aca10ab5b47b622dc0b3866879e8e4c9e5e75
soozynn <junsuejin98@gmail.com>
2026-02-25 17:11:52 +0900

    Refactor(_layout): change the RootLayout metadata image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,24 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Portfolio | Frontend Developer',
-  description: 'Personal portfolio website',
+  title: 'soozynn"s Portfolio | Frontend Developer',
+  description: 'soozynn"s portfolio website',
+  openGraph: {
+    title: 'Soozynn Portfolio',
+    description: 'Frontend Developer Portfolio',
+    url: 'https://portfolio-soozynn.vercel.app',
+    siteName: 'Soozynn Portfolio',
+    images: [
+      {
+        url: 'https://portfolio-soozynn.vercel.app/profile.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Soozynn Portfolio Profile Image',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
